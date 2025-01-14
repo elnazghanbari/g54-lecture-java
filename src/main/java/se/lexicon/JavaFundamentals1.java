ec009f0cdd9e649886c5375520f06f4b13fe86f1
Elnaz <elnaaz.g@gmail.com>
2025-01-14 23:47:11 +0100

    java fundamentals demo

---
+++ src/main/java/se/lexicon/JavaFundamentals1.java
@@ -0,0 +1,28 @@
+package se.lexicon;
+
+import java.lang.classfile.instruction.TableSwitchInstruction;
+import java.util.Scanner;
+
+public class JavaFundamentals1 {
+
+    //Java comments
+    //This is a single line comment
+    /*
+    This is a regular multi-line comment
+    add multiple lines
+     */
+
+    /**
+     * This is a Java Doc (Documentation Comments)
+     * Used to create Documentations for the java codes
+     */
+
+    //psvm + Tab
+    public static void main(String[] args) {
+        // sout + Tab
+        System.out.println("Hello World!");
+
+        //## Variable Declaration ##
+
+    }
+}
